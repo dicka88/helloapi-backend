b5ab0d34a1284bf7ce7434d84b47ff14557d4360
Dicka Ismaji <dickaismaji@gmail.com>
2022-03-30 01:18:49 +0700

    disable POST,PUT and, DELETE

--- src/routes/api.routes.ts
+++ src/routes/api.routes.ts
@@ -5,9 +5,9 @@ import {
 
 const routes: FastifyPluginAsync = async (app): Promise<void> => {
   app.get('/:prefixPath/:path', {}, getHandler);
-  app.post('/:prefixPath/:path', {}, postHandler);
-  app.put('/:prefixPath/:path', {}, putHandler);
-  app.delete('/:prefixPath/:path', {}, deleteHandler);
+  // app.post('/:prefixPath/:path', {}, postHandler);
+  // app.put('/:prefixPath/:path', {}, putHandler);
+  // app.delete('/:prefixPath/:path', {}, deleteHandler);
 };
 
 export default routes;
